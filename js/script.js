9f42d3a4b74834eaadbd45da20cf5a9d173ca0bc
Nikita <nborisov256@gmail.com>
2018-09-07 12:59:17 +0300

    14

--- js/script.js
+++ js/script.js
@@ -130,4 +130,20 @@ let portfolioMenu = document.querySelector('.portfolio-menu'),
 
 filter();
 
+
+//gift timeout
+function giftTimeout() {
+	let popupDesign = document.querySelector('.popup-design'),
+		popupConsultation = document.querySelector('.popup-consultation'),
+		popupGift = document.querySelector('.popup-gift');
+
+	let	consultationTimeout = setTimeout(function() {
+		if (popupDesign.style.display !== 'block' && popupGift.style.display !== 'block') {
+			popupConsultation.style.display = 'block';
+		}
+	},60000)
+};
+
+giftTimeout();
+
 })
