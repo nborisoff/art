e70df8264df71a54d1f2fcabf3f9bf90bf65ff04
Nikita <nborisov256@gmail.com>
2018-09-07 13:41:20 +0300

    10

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-//slider
+//main slider
 function mainSlider() {
 	let slideIndex = 1,
 		slides = document.getElementsByClassName('main-slider-item'),
+
 		slideInterval = setInterval(function() {
 			showSlides(slideIndex += 1)
 		}, 5000);	
@@ -145,5 +146,49 @@ function giftTimeout() {
 };
 
 giftTimeout();
+
+//feedback slider
+function feedbackSlider() {
+	let slideIndex = 1,
+		slides = document.getElementsByClassName('feedback-slider-item'),
+		prev = document.querySelector('.main-prev-btn'),
+		next = document.querySelector('.main-next-btn'),
+
+		slideInterval = setInterval(function() {
+			showSlides(slideIndex += 1)
+		}, 15000);	
+	
+	showSlides(slideIndex);
+
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
+
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].style.display = 'none';
+		}
+
+		slides[slideIndex - 1].style.display = 'block';
+	};
+
+	function plusSlides(n) {
+		showSlides(slideIndex += n);
+	};
+
+	prev.addEventListener('click', function() {
+		plusSlides(-1);
+	});
+
+	next.addEventListener('click', function() {
+		plusSlides(1);
+	});
+}
+
+feedbackSlider();
 
 })
