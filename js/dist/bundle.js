2aa30a11182c9620fb1e6e39769dfe935159929d
Nikita <nborisov256@gmail.com>
2018-09-12 19:20:42 +0300

    last

--- js/dist/bundle.js
+++ js/dist/bundle.js
@@ -8095,7 +8095,7 @@ function styles() {
       styleClass = document.getElementsByClassName('styles-2');
   moreBtn.addEventListener('click', function () {
     for (var i = 0; i < styleClass.length; i++) {
-      styleClass[i].classList = 'col-sm-3 col-sm-offset-0 col-xs-10 col-xs-offset-1 styles-2';
+      styleClass[i].className = 'col-sm-3 col-sm-offset-0 col-xs-10 col-xs-offset-1 styles-2';
     }
 
     this.style.display = "none";
